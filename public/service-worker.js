ee4619a5757af94ce0ae0377d3b15a2f57f96be1
Daniel O <danielogunlana23@gmail.com>
2021-07-23 13:13:45 -0400

    fixed indexdb database

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,10 +4,12 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/index.js",
+  "/db.js",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "/styles.css"
+  
 ];
 
 // install
